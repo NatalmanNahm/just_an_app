8e320bfa93adb9033040d13b3afd397879c8f77a
Natalman <natalnahm@gmail.com>
2020-03-14 11:47:34 -0500

    added NetwokUtil. java with methods to build url, make http request and read the json file. added a category list item that hold view for each category and added a recyclerview to the main activity xml file.

---
+++ app/src/main/java/com/example/just_an_app/utilities/NetworkUtil.java
@@ -0,0 +1,119 @@
+package com.example.just_an_app.utilities;
+
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+/**
+ * Utility class that will serves as network requester to the data base
+ * https://opentdb.com
+ */
+
+public class NetworkUtil {
+
+    private static final String TAG = NetworkUtil.class.getSimpleName();
+    //Base URL
+    private static final String baseUrl = "https://opentdb.com";
+    //Other path
+    private static final String CATEGORY = "api_category.php";
+
+    /**
+     * this is the method that build the category url
+     * @return url
+     */
+    private static URL build_all_categories(){
+
+        //Build the URL with the query parameter
+        Uri uriBuilder = Uri.parse(baseUrl).buildUpon()
+                .appendPath(CATEGORY)
+                .build();
+
+        URL url = null;
+
+        try {
+            url = new URL(uriBuilder.toString());
+        }catch (MalformedURLException e){
+            Log.e(TAG, "Error building Url: " + url);
+        }
+
+        return url;
+    }
+
+    /**
+     * Convert the {@link InputStream} into a String which contains the
+     * whole JSON response from the server.
+     */
+    private static String readFromStream(InputStream inputStream) throws IOException{
+        StringBuilder output = new StringBuilder();
+
+        if (inputStream != null){
+            InputStreamReader inputStreamReader =
+                    new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            String line = bufferedReader.readLine();
+
+            while (line != null){
+                output.append(line);
+                line = bufferedReader.readLine();
+            }
+        }
+        return output.toString();
+    }
+
+    /**
+     * This method returns the entire result from the HTTP response.
+     *
+     * @param url The URL to fetch the HTTP response from.
+     * @return The contents of the HTTP response.
+     * @throws IOException Related to network and stream reading
+     */
+    private static String getResponseFromHttpUrl(URL url) throws IOException{
+        String jsonResponse = "";
+
+        if (url == null){
+            return jsonResponse;
+        }
+
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+
+        //Trying to get connection the server and if the status is 200
+        //That means the connection was a success and then we can retrieve data needed
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(10000);
+            urlConnection.setConnectTimeout(15000);
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            if (urlConnection.getResponseCode() == 200){
+                inputStream = urlConnection.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+            } else {
+                Log.e(TAG, "Build error code: " + urlConnection.getResponseCode());
+            }
+        }catch (IOException e){
+            Log.e(TAG, "MakeHttpRequest: Error retrieving data from Json result" + e);
+        }finally {
+            if (urlConnection != null){
+                urlConnection.disconnect();
+            }
+            if (inputStream != null){
+                inputStream.close();
+            }
+        }
+        return jsonResponse;
+    }
+
+
+
+
+}
