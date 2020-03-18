0a5ebe3e05db862e39273031d4f52f555f237426
Natalman <natalnahm@gmail.com>
2020-03-17 19:18:09 -0500

    add Parcelable to the categories model, and made sure on screen rotation we retore back to the place we left off.

--- app/src/main/java/com/example/just_an_app/MainActivity.java
+++ app/src/main/java/com/example/just_an_app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.just_an_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -7,6 +8,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.util.Log;
 
 import com.example.just_an_app.Adapters.AllCategoriesAdapter;
@@ -22,9 +24,11 @@ public class MainActivity extends AppCompatActivity implements AllCategoriesAdap
     private AllCategoriesAdapter mCategoryAdapter;
     private ArrayList<Categories> mCategories;
     private LinearLayoutManager mLinearlayoutManager;
+    private Parcelable mSavedLineearLayout;
     private static final String CATID = "id";
     private static final String NAME = "name";
     private static final String BUNDLE = "bundle";
+    private static final String SAVED_INSTANCE_KEY = "position";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +43,13 @@ public class MainActivity extends AppCompatActivity implements AllCategoriesAdap
 
         mCategoryAdapter = new AllCategoriesAdapter(getApplicationContext(), mCategories, this);
         mRecyclerView.setAdapter(mCategoryAdapter);
+
+        //This is to ensure that whenever the screen is rotated or we are back to this activity
+        //we always get back to where we left off.
+        if (savedInstanceState != null){
+            mSavedLineearLayout = savedInstanceState.getParcelable(SAVED_INSTANCE_KEY);
+            mLinearlayoutManager.onRestoreInstanceState(mSavedLineearLayout);
+        }
 
         new FetchAllCategories().execute();
     }
@@ -63,10 +74,17 @@ public class MainActivity extends AppCompatActivity implements AllCategoriesAdap
 
             if (categories != null && !categories.isEmpty()){
                 mCategoryAdapter.setCategories(categories);
+                mLinearlayoutManager.onRestoreInstanceState(mSavedLineearLayout);
             }
         }
     }
 
+    /**
+     * On click of an item start an intent an pass data (id and name)
+     * to the activity we open.
+     * @param id
+     * @param name
+     */
     @Override
     public void onClick(int id, String name) {
         Intent intent = new Intent(MainActivity.this, QuestionActivity.class);
@@ -75,5 +93,11 @@ public class MainActivity extends AppCompatActivity implements AllCategoriesAdap
         bundle.putString(NAME, name);
         intent.putExtra(BUNDLE, bundle);
         startActivity(intent);
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putParcelable(SAVED_INSTANCE_KEY, mLinearlayoutManager.onSaveInstanceState());
     }
 }
--- app/src/main/java/com/example/just_an_app/Models/Categories.java
+++ app/src/main/java/com/example/just_an_app/Models/Categories.java
@@ -1,10 +1,13 @@
 package com.example.just_an_app.Models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Model for all categories
  */
 
-public class Categories {
+public class Categories implements Parcelable {
 
     //initializer
     private int mCatId;
@@ -19,6 +22,34 @@ public class Categories {
         this.mCatId = id;
         this.mCategory = category;
     }
+
+    private Categories(Parcel in) {
+        mCatId = in.readInt();
+        mCategory = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(mCatId);
+        dest.writeString(mCategory);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Categories> CREATOR = new Creator<Categories>() {
+        @Override
+        public Categories createFromParcel(Parcel in) {
+            return new Categories(in);
+        }
+
+        @Override
+        public Categories[] newArray(int size) {
+            return new Categories[size];
+        }
+    };
 
     public int getmCatId() {
         return mCatId;
