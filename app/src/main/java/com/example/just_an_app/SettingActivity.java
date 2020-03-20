be7b5a4f526b119f498cc417493a8e7d169c3623
Natalman <natalnahm@gmail.com>
2020-03-19 19:16:28 -0500

    modified the setting activity so that on home button pressed we go back to the main activity.

--- app/src/main/java/com/example/just_an_app/SettingActivity.java
+++ app/src/main/java/com/example/just_an_app/SettingActivity.java
@@ -1,7 +1,12 @@
 package com.example.just_an_app;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
+
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class SettingActivity extends AppCompatActivity {
 
@@ -9,5 +14,23 @@ public class SettingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        ActionBar actionBar = this.getSupportActionBar();
+
+        // Set the action bar back button to look like an up button
+        if (actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        //When home button is pressed take the user back to the Main activity
+        if (id == android.R.id.home){
+            NavUtils.navigateUpFromSameTask(this);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
