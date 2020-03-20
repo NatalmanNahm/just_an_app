8c4b8e0506d0a572c55267078154f0655b31623d
Natalman <natalnahm@gmail.com>
2020-03-19 19:03:42 -0500

    Added SettingFragment whih handles whatever happens inside the setting page. Added an acticty for the setting too.

---
+++ app/src/main/java/com/example/just_an_app/SettingFragment.java
@@ -0,0 +1,115 @@
+package com.example.just_an_app;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.preference.CheckBoxPreference;
+import androidx.preference.EditTextPreference;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceScreen;
+
+public class SettingFragment extends PreferenceFragmentCompat implements
+        SharedPreferences.OnSharedPreferenceChangeListener, Preference.OnPreferenceChangeListener {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        //add reference to the xml file
+        addPreferencesFromResource(R.xml.pref_visualizer);
+
+        SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
+        PreferenceScreen preferenceScreen = getPreferenceScreen();
+        int count = preferenceScreen.getPreferenceCount();
+
+        // Go through all of the preferences, and set up their preference summary.
+        for (int i = 0; i<count; i++){
+            Preference p = preferenceScreen.getPreference(i);
+
+            if (!(p instanceof CheckBoxPreference)){
+                String value = sharedPreferences.getString(p.getKey(), "");
+                setPreferenceSummary(p, value);
+            }
+        }
+
+        // Add the preference listener which checks that the size is correct to the size preference
+        Preference preference = findPreference(getString(R.string.pref_num_key));
+        preference.setOnPreferenceChangeListener(this);
+    }
+
+    private void setPreferenceSummary(Preference preference, String value) {
+
+        if (preference instanceof ListPreference){
+            //For the listPreferences, figure out the label of the selected value
+            ListPreference listPreference = (ListPreference) preference;
+            int prefIndex = listPreference.findIndexOfValue(value);
+            if (prefIndex >= 0){
+                //set Summary of the label
+                listPreference.setSummary(listPreference.getEntries() [prefIndex]);
+            }
+        } else if (preference instanceof EditTextPreference){
+            // For EditTextPreferences, set the summary to the value's simple string representation.
+            preference.setSummary(value);
+        }
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        //figure out which of the preference was changed
+        Preference preference = findPreference(key);
+
+        if (preference != null){
+            //update the preference summary
+            if (!(preference instanceof CheckBoxPreference)){
+                String value = sharedPreferences.getString(preference.getKey(), "");
+                setPreferenceSummary(preference,value);
+            }
+        }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        // In this context, we're using the onPreferenceChange listener for checking whether the
+        // number of questions setting was set to a valid value.
+
+        Toast error = Toast.makeText(getContext(), "please select number between 5 and 30",
+                Toast.LENGTH_SHORT);
+
+        //Double check that we get the right preference
+        String key = getString(R.string.pref_num_key);
+
+        if (preference.getKey().equals(key)){
+            String stringKey = (String) newValue;
+
+            try {
+                int number = Integer.parseInt(stringKey);
+                // If the number is outside of the acceptable range, show an error.
+                if (number > 30 || number <= 5){
+                    error.show();
+                    return false;
+                }
+            }catch (NumberFormatException nfe){
+                // If whatever the user entered can't be parsed to a number, show an error
+                error.show();
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+}
